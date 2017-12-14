7cd580e5b72bffd9cc1c0f7f5341e22a39f811a3
MorganStessman <morganstessman@sbcglobal.net>
2017-12-14 00:46:04 -0600

    Defining Functions

--- ArrowControls.js
+++ ArrowControls.js
@@ -1,3 +1,5 @@
+/* You're a wizard Harry.
+
 /*
 Functions in this file:
 keys(e)           Keyboard controls
@@ -55,4 +57,63 @@ else if{
 (e.keyCode == 32){
 animationToggle();
 return !(e.keyCode == 32);
+}
+
+function prevProduct(){
+if(dprog_urls.length == 0){
+if(hr - interval >= minhour){
+if(hr <= 0){hr = hr - models[model_idx].run_interval;}
+else if(hr > 0){hr = hr - interval;}
+}
+
+//Update image display
+showImage();
+}
+else{
+element = document.getElementById('slider-dprog');
+if(parseInt(element.value) > 0){
+element.value = parseInt(element.value) - 1;
+}
+document.dprogmap.src = dprog_urls[element.value].src;
+}
+}
+
+//Step one interval forward
+function nextProduct(){
+if(dprog_urls.length == 0){
+if(hr + interval <= maxhour){
+if(hr < 0){hr = hr + models[model_idx].run_interval;}
+else if(hr >= 0){hr = hr + interval;}
+}
+
+//Update image display
+showImage();
+}
+else{
+element = document.getElementById('slider-dprog');
+if(parseInt(element.value) < dprog_urls.length){
+element.value = parseInt(element.value) + 1;
+}
+document.dprogmap.src = dprog_urls[element.value].src;
+}
+}
+
+//Up one level
+function prevStep(){
+//Current index of product
+var curprod_idx = searchByName(prod,curprods)
+//Move up by one
+if(curprod_idx > 0){
+changeProd(curprods[curprod_idx-1].name)
+}
+}
+
+//Down one level
+function nextStep(){
+//Current index of product
+var curprod_idx = searchByName(prod,curprods)
+//Move up by one
+if(curprod_idx < curprods.length-1){
+changeProd(curprods[curprod_idx+1].name)
+}
 }
